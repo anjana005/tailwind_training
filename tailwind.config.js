6a7ad72952a3a24283b18da048465772054f3d32
Anjana <anjana@vuedata.in>
2022-10-09 13:24:56 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge:["*.html"],
+  content: ["*.html"],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {
+
+      backgroundColor:["active"],
+      
+    },
+  },
+  plugins: [],
+}
